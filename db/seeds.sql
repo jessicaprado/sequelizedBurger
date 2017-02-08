c47580d88f515d77c489b6a5d272ed6cf1c286f4
Jessica Prado <jessica.marie.prado@gmail.com>
2017-02-07 22:22:48 -0500

    syntax error

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burger (burger_name, devoured) VALUES ('Lettuce Wrapped Burger', true);
-INSERT INTO burger (burger_name, devoured) VALUES ('Cheese Burger', false);
-INSERT INTO burger (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lettuce Wrapped Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
