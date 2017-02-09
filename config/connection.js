35481c561659b7461b59fdf87c69bb2803d93afe
Jessica Prado <jessica.marie.prado@gmail.com>
2017-02-08 23:02:08 -0500

    heroki

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,27 @@
 var Sequelize = require('sequelize');
+var sequelize;
+//var mysql = require('mysql');
+var env;
+if (process.env.JAWSDB_URL)
+    env = "production";
+else {
+    env = 'development';
+}
+var connection;
+var config = require('./config')[env];
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+    sequelize = new Sequelize(config.database, config.username, config.password, {
+        host: config.host,
+        dialect: config.dialect
+    });
+    //     host: "127.0.0.1",
+    //     user: "root",
+    //     password: "root",
+    //     database: "burgers_db"
+    // });
+};
 
-var sequelize = new Sequelize('burger_db', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
 
 module.exports = sequelize;
